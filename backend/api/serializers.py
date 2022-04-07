220b6f43107c0d978c7ec01f3021462cc9a5b795
Vlad Nikitin <nikitinv91@gmail.com>
2022-04-07 12:21:55 +0400

    git

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.auth import password_validation
 from django.db.models import F
 from rest_framework import serializers
 
+from api.fields import Base64ImageField
 from api.models import Tag, Recipe, Ingredient
 from users.models import CustomUser, Follow
 
@@ -105,39 +106,6 @@ class IngredientSerializer(serializers.ModelSerializer):
         model = Ingredient
         fields = '__all__'
         read_only_fields = ('__all__',)
-
-
-class Base64ImageField(serializers.ImageField):
-
-    def to_internal_value(self, data):
-        from django.core.files.base import ContentFile
-        import base64
-        import six
-        import uuid
-
-        if isinstance(data, six.string_types):
-            if 'data:' in data and ';base64,' in data:
-                header, data = data.split(';base64,')
-
-            try:
-                decoded_file = base64.b64decode(data)
-            except TypeError:
-                self.fail('invalid_image')
-
-            file_name = str(uuid.uuid4())[:12]
-            file_extension = self.get_file_extension(file_name, decoded_file)
-            complete_file_name = "%s.%s" % (file_name, file_extension, )
-            data = ContentFile(decoded_file, name=complete_file_name)
-
-        return super(Base64ImageField, self).to_internal_value(data)
-
-    def get_file_extension(self, file_name, decoded_file):
-        import imghdr
-
-        extension = imghdr.what(file_name, decoded_file)
-        extension = "jpg" if extension == "jpeg" else extension
-
-        return extension
 
 
 class RecipeSerializer(serializers.ModelSerializer):
