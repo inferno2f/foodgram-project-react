ea0e17dd893cf4680adf80d6afe2059dd5cdb393
Vlad Nikitin <nikitinv91@gmail.com>
2022-04-25 13:30:37 +0300

    devops madness

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -122,11 +122,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
+STATIC_URL = '/backend_static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/backend_media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
